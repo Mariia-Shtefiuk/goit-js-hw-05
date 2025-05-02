8fbd520cd70884d64f9b6c4d187932fb3c4f1d12
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-05-02 17:38:17 +0300

    js folder update

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,45 @@
-const blacklist1 = 'spam';
-const blacklist2 = 'sale';
+const sortByDescendingFriendCount = users => {
+  return users
+    .toSorted((a, b) => b.friends.length - a.friends.length)
+    .map(user => user.name);
+};
 
-function checkForSpam(message) {
-  if (
-    message.toLowerCase().includes(blacklist1) ||
-    message.toLowerCase().includes(blacklist2)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(checkForSpam('Latest technology news'));
-console.log(checkForSpam('JavaScript weekly newsletter'));
-console.log(checkForSpam('Get best sale offers now!'));
-console.log(checkForSpam('Amazing SalE, only tonight!'));
-console.log(checkForSpam('Trust me, this is not a spam message'));
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
-console.log(checkForSpam('[SPAM] How to earn fast money?'));
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
+    },
+    {
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
+    },
+    {
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
+    },
+    {
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
+    },
+    {
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
+    },
+    {
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
+    },
+    {
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
+  ])
+);
